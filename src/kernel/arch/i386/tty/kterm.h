394882ca2e2f4124a9370a4bbb30c9bb8c7d2c1a
Nigel <nigelbarink@hotmail.com>
2021-05-18 21:11:48 +0100

    Added CMOS time read function, Added cariage return support to kterm

--- src/kernel/arch/i386/tty/kterm.h
+++ src/kernel/arch/i386/tty/kterm.h
@@ -15,10 +15,16 @@ void kterm_putat(char, uint8_t, size_t, size_t);
 void kterm_put(char);
 void kterm_write(const char*, size_t);
 void kterm_writestring(const char*);
-
 void kterm_scrollup();
+
+
+void printf ( const char *format, ...);
+
+static void itoa (char *buf, int base, int d);
 
 #define KernelTag "[Kernel]: "
 #define AS_KERNEL() (  kterm_setcolor(VGA_COLOR_LIGHT_BLUE),\
                        kterm_write(KernelTag, 10 ), \
                        kterm_resetcolor())
+
+
