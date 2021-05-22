195e67a45a3d6f86c9fb049e95051c744b190dfb
Nigel <nigelbarink@hotmail.com>
2021-05-22 21:54:18 +0100

    SerialHub CPP Object added

--- src/kernel/arch/i386/ports/serial.h
+++ src/kernel/arch/i386/ports/serial.h
@@ -1,19 +1,43 @@
 #pragma once
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-class Serial {
+extern "C" {
+    #include "../tty/kterm.h"
+}
+#include "../../../io.h"
+
+extern "C" void kterm_writestring(const char* );
+
+class SerialHub {
 
     public: 
-        static Serial init();
         
-        void print();
+        SerialHub();
+
+        ~SerialHub();
+
+
+        void print(const char* message);
+        void println(const char* message);
+
+        inline void setPort (const int* port ){PORT = *port;}
 
     private: 
         const int COM1 = 0x3F8;
         const int COM2 = 0x2F8;
         const int COM3 = 0x3E8;
         const int COM4 = 0x2E8;
+        
+        int PORT;
+
+        int init(int);
+        int is_transmit_empty();
+        void write_character(char a);
+        int received();
+        char read();
+        void test();
 
 
-        Serial();
-        ~Serial();
 };
